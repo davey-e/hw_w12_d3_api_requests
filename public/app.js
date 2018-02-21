2115ddb8fbbfd3f4f616a50eedfe86bb5469af76
Dave Ellis <dave@ellisnetmail.co.uk>
2018-02-21 19:49:31 +0000

    Added code to get beer data from api and display a list of beers

--- public/app.js
+++ public/app.js
@@ -1,5 +1,36 @@
 const app = function () {
 
-};
+    const url = "https://api.punkapi.com/v2/beers";
+    // const url = "https://s3-eu-west-1.amazonaws.com/brewdogapi/beers.json"
+
+    makeRequest(url, requestComplete);
+}
+
+const makeRequest = function(url, callback){
+    const request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.addEventListener("load", callback);
+    request.send();
+}
+
+const requestComplete = function(){
+    if(this.status !== 200) return;
+    const jsonString = this.responseText;
+    const beers = JSON.parse(jsonString);
+    populateList(beers);
+}
+
+const populateList = function(beersList){
+    const beerListUl = document.getElementById("beers-list")
+    beersList.forEach(function(beer){
+        addBeerToList(beer, beerListUl);
+    });
+}
+
+const addBeerToList = function(beer, beerListUlElement){
+    const li = document.createElement('li');
+    li.innerText = beer.name;
+    beerListUlElement.appendChild(li);
+}
 
 document.addEventListener('DOMContentLoaded', app);
