9c29ceb7598913252d69b40bd775ebe56a5f7c99
Dave Ellis <dave@ellisnetmail.co.uk>
2018-02-21 21:36:33 +0000

    Added a button to view all beers, which will be used to switch from viewing single beer details back to viewing all beers

--- public/app.js
+++ public/app.js
@@ -3,14 +3,18 @@ const app = function () {
     const url = "https://api.punkapi.com/v2/beers";
     // const url = "https://s3-eu-west-1.amazonaws.com/brewdogapi/beers.json"
 
-    makeRequest(url, requestComplete);
-}
+    const makeRequest = function(){
+        const table = document.getElementById("beer-list");
+        table.innerHTML = "";
+        const request = new XMLHttpRequest();
+        request.open("GET", url);
+        request.addEventListener("load", requestComplete);
+        request.send();
+    }
 
-const makeRequest = function(url, callback){
-    const request = new XMLHttpRequest();
-    request.open("GET", url);
-    request.addEventListener("load", callback);
-    request.send();
+    const viewAllBeersButton = document.getElementById("view-all-button");
+    viewAllBeersButton.addEventListener('click', makeRequest);
+    
 }
 
 const requestComplete = function(){
@@ -23,19 +27,19 @@ const requestComplete = function(){
 const populateList = function(beersList){
     const table = document.getElementById("beer-list");
     beersList.forEach(function(beer){
-        addBeerToList(beer, table);
+        addBeerToTable(beer, table);
     });
 }
 
-const addBeerToList = function(beer, tableElement){
+const addBeerToTable = function(beer, tableElement){
     const img = document.createElement("img");
     img.src = beer.image_url;
-    // img.height = 200;
     const tr = document.createElement("tr");
     const td1 = document.createElement("td");
-    const td2 = document.createElement("td");
     td1.appendChild(img);
+    const td2 = document.createElement("td");
     td2.innerText = beer.name;
+    td2.id = beer.id;
     tr.appendChild(td1);
     tr.appendChild(td2);
     tableElement.appendChild(tr);
