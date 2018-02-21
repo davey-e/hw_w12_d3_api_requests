419e03080e5097a1fe35a31d264e32b7cd261e33
Dave Ellis <dave@ellisnetmail.co.uk>
2018-02-21 20:25:50 +0000

    Changed display of beers to use a table instead of ul and improved styling

--- public/app.js
+++ public/app.js
@@ -21,20 +21,24 @@ const requestComplete = function(){
 }
 
 const populateList = function(beersList){
-    const beerListUl = document.getElementById("beers-list")
+    const table = document.getElementById("beer-list");
     beersList.forEach(function(beer){
-        addBeerToList(beer, beerListUl);
+        addBeerToList(beer, table);
     });
 }
 
-const addBeerToList = function(beer, beerListUlElement){
-    const li = document.createElement("li");
-    li.innerText = beer.name;
-    beerListUlElement.appendChild(li);
+const addBeerToList = function(beer, tableElement){
     const img = document.createElement("img");
     img.src = beer.image_url;
-    img.height = 200;
-    li.appendChild(img);
+    // img.height = 200;
+    const tr = document.createElement("tr");
+    const td1 = document.createElement("td");
+    const td2 = document.createElement("td");
+    td1.appendChild(img);
+    td2.innerText = beer.name;
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tableElement.appendChild(tr);
 }
 
 document.addEventListener('DOMContentLoaded', app);
