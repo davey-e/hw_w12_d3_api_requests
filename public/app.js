2b7a5300a17ca01c23397a4394b75a3fa7128c7b
Dave Ellis <dave@ellisnetmail.co.uk>
2018-02-21 23:31:06 +0000

    Added code to display beer details after making a slection from the drop-down

--- public/app.js
+++ public/app.js
@@ -12,10 +12,56 @@ const app = function () {
         request.send();
     }
 
+    const handleDDSelectionChange = function(){
+        const table = document.getElementById("beer-list");
+        table.innerHTML = "";
+        const id = parseInt(viewBeerDetailsDD.value) + 1;
+        const urlWithId = "https://api.punkapi.com/v2/beers/" + id;
+        const requestSingle = new XMLHttpRequest();
+        requestSingle.open("GET", urlWithId);
+        requestSingle.addEventListener("load", requestCompleteSingle);
+        requestSingle.send();
+    }
+
     const viewAllBeersButton = document.getElementById("view-all-button");
     viewAllBeersButton.addEventListener("click", makeRequest);
+
+    const viewBeerDetailsDD = document.getElementById("select-beer");
+    viewBeerDetailsDD.addEventListener("change", handleDDSelectionChange);
     
 }
+
+const requestCompleteSingle = function(){
+    if(this.status !== 200) return;
+    const jsonString = this.responseText;
+    const beer = JSON.parse(jsonString);
+    populateTableSingle(beer);
+}
+
+const populateTableSingle = function(beer){
+    console.log(beer);
+    
+    const table = document.getElementById("beer");
+    table.innerHTML = "";
+
+    let itemsToDisplay = [];
+    itemsToDisplay.push("name");
+    itemsToDisplay.push("tagline");
+    itemsToDisplay.push("abv");
+    itemsToDisplay.push("food_pairing");
+    
+    itemsToDisplay.forEach(function(key){
+        const tr1 = document.createElement("tr");
+        const td1 = document.createElement("td");
+        td1.innerText = key + ":";
+        const td2 = document.createElement("td");
+        td2.innerText = beer[0][key];
+        tr1.appendChild(td1);
+        tr1.appendChild(td2);
+        table.appendChild(tr1);
+    })
+}
+
 
 const requestComplete = function(){
     if(this.status !== 200) return;
@@ -54,14 +100,15 @@ const populateDDList = function(beersList){
     disabledOption.selected = true;
     disabledOption.innerText = "Select a Beer from the list to view its details";
     beerDetailsDropDown.appendChild(disabledOption);
-    beersList.forEach(function(beer){
-        addBeerToList(beer, beerDetailsDropDown);
+    beersList.forEach(function(beer, index){
+        addBeerToList(beer, index, beerDetailsDropDown);
     });
 }
 
-const addBeerToList = function(beer, dropDown){
+const addBeerToList = function(beer, index, dropDown){
     const option = document.createElement("option");
     option.innerText = beer.name;
+    option.value = index;
     dropDown.appendChild(option);
 }
 
